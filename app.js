70aac321cd7e9bced962402030392eb1d5da7466
Alex Withington-Smith <alex@awithsmith.com>
2021-03-10 16:34:11 +0000

    added database connection

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+const connectToDatabase = require("./config/connect-db");
 const server = require("./server/server");
 const port = process.env.PORT || 5000;
+
+require("dotenv").config();
+
+connectToDatabase();
 
 server.listen(port, () => {
   console.log(`App is listening on port ${port}`);
