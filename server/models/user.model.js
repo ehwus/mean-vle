43ba8f65e6c923c7abad0ba67e2fc72ded4f25ac
Alex Withington-Smith <alex@awithsmith.com>
2021-03-10 17:10:48 +0000

    added user schema

---
+++ server/models/user.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  dateAccountCreated: {
+    type: Date,
+    default: Date.new,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  userClass: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
